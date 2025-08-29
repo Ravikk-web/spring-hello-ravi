b1768971c7f71323622da0829f461d95ea4ee50a
Ravi K. Kispotta <117717050+Ravikk-web@users.noreply.github.com>
2025-08-29 07:30:10 +0530

    Update HomeController.java

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from Ravikant Kispotta!";
+        return "Hello from Ravi Kant Kispotta!";
     }
 
 }
